0f9ba6ac848bf1b7cd0602f8e9b21db77efc5d4a
anand <anandvipul01@gmail.com>
2022-09-23 12:54:18 +0530

    Assignment: DOM Manipulation

--- block-BJaadz/code/script.js
+++ block-BJaadz/code/script.js
@@ -1,44 +1,79 @@
 // Select the h1 element and store it in a variable named heading.
-
+let heading = document.querySelector("h1");
 // Check the typeof heading and log it.
-
+console.log(typeof heading);
 // Change the color of heading to black.
-
+heading.setAttribute(`style`, `color:black`);
 // Select all the hr elements and store it in a variable named allHrs using querySelectorAll
-
+let allHrs = document.querySelectorAll(`hr`);
 /* 
 Convert the NodeList returned by querySelectorAll to Array using Array.from() or spread operator and store it in allHrsArray
-
 Array.from converts an array kind of data into array so we can use methods like map, reduce
-
 HINT:
 let allHrsArray = Array.from(allHrs)
 */
-
+let allHrsArray = Array.from(allHrs);
 // Set the border of the all the hr elements to "1px solid tomato"
-
+allHrsArray.forEach((elem) => {
+  elem.setAttribute("style", "border:1px solid tomato");
+});
 // Change the background of all the hr to "antiquewhite" using for of loop.
-
+for (var elem of allHrsArray) {
+  elem.setAttribute(
+    "style",
+    elem.getAttribute("style") + `; background-color: antiquewhite`
+  );
+}
 // Change the 'border-radius' of all the hr to "5px" using array.
-
+for (var elem of allHrsArray) {
+  elem.setAttribute(
+    "style",
+    elem.getAttribute("style") + `; border-radius: 5px`
+  );
+}
 // Change the alignment of the heading(h1) to center.
-
+heading.setAttribute(
+  "style",
+  heading.getAttribute("style") + "; text-align: center;"
+);
 // Change the font size of the heading to 3rem.
-
+heading.setAttribute(
+  "style",
+  heading.getAttribute("style") + "; font-size: 3rem;"
+);
 // Change the border of hr with class 'image' to `2px solid purple`.
 
+function changeProperty(selectedElements, property, frm, to) {
+  selectedElements.forEach((elem) => {
+    elem.setAttribute(
+      `${property}`,
+      elem.getAttribute(`${property}`).replace(`${frm}`, `${to}`)
+    );
+  });
+}
+
+changeProperty(
+  document.querySelectorAll("hr.image"),
+  "style",
+  "1px solid tomato",
+  "2px solid purple"
+);
+
 // Hide the box number 17 (last box).
-
+document
+  .querySelector("div.box.seventeen")
+  .setAttribute("style", "display:none");
 // Change the border of all the hr element from solid to dashed type
-
+changeProperty(document.querySelectorAll("hr"), "style", "solid", "dashed");
 // Create a pragraph element and store it in variable named 'para' using `createElement`
-
+let para = document.createElement("p");
 // Change the inner text of para to "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure."
-
+para.innerText =
+  "querySelector returns an element you can maupulate but querySelectorAll returns the collection of elements in array kind of structure.";
 // Remove all the elements from box 1
-
+document.querySelector("div.box.one").innerHTML = "";
 // Replace all the elements inside box 1 with the para (you created above)
-
+document.querySelector("div.box.one").innerHTML = para;
 /* Walking the DOM
 Do the following after selecting box 16 and storing in variable named box16
 
